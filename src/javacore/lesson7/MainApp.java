5bfbeb88a7493ebdb9f74ea0900f527e87768c12
Subudei <Subudei@gmail.com>
2022-10-13 22:16:51 +0600

    lesson7

---
+++ src/javacore/lesson7/MainApp.java
@@ -0,0 +1,9 @@
+package lesson7.project;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        lesson7.project.UserInterface userInterface = new lesson7.project.UserInterface();
+        userInterface.runApplication();
+    }
+}
--- src/javacore/lesson7/WeatherResponse.java
+++ src/javacore/lesson7/WeatherResponse.java
@@ -1,8 +1,15 @@
-package javacore.lesson7;
+package lesson7.project;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonRootName(value = "MyDailyWeather")
 public class WeatherResponse {
 
-    private DailyForecasts[] forecasts = new DailyForecasts[5];
+    @JsonProperty("DailyForecasts")
+    private DailyForecasts[] forecasts;
 
     public DailyForecasts[] getForecasts() {
         return forecasts;
@@ -11,4 +18,44 @@ public class WeatherResponse {
     public void setForecasts(DailyForecasts[] forecasts) {
         this.forecasts = forecasts;
     }
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class DailyForecasts{
+
+    @JsonProperty("Date")
+    public String date;
+
+    @JsonProperty("Temperature")
+    public Temperature2 temperature;
+
+    @JsonProperty("Day")
+    public Day day;
+
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Temperature2 {
+    @JsonProperty("Minimum")
+    public Minimum minimum;
+    @JsonProperty("Maximum")
+    public Maximum maximum;
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Minimum {
+    @JsonProperty("Value")
+    public int value;
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Maximum {
+    @JsonProperty("Value")
+    public int value;
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Day {
+    @JsonProperty("IconPhrase")
+    public String iconPhrase;
 }
